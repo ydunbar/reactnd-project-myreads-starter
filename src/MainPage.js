52240a3989acec843f49c97f3099e4a2d248b28d
Yolotzin Dunbar <yltzndnbr@gmail.com>
2018-10-01 17:18:28 -0700

    Added filters for bookshelves

--- src/MainPage.js
+++ src/MainPage.js
@@ -15,10 +15,17 @@ class MainPage extends Component {
                   <h2 className="bookshelf-title">Currently Reading</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-                      <li>
+                     {
+                    	this.props.books
+                    	.filter(book => book.shelf === 'currentlyReading')
+                    	.map(book => (
+                    	<li key={book.id}>
                       <Book />
-                     
                       </li>
+                    	))
+                    }
+
+                      
                     </ol>
                   </div>
                 </div>
@@ -26,11 +33,16 @@ class MainPage extends Component {
                   <h2 className="bookshelf-title">Want to Read</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-                      <li>
+                    {
+                    	this.props.books
+                    	.filter(book => book.shelf === 'wantToRead')
+                    	.map(book => (
+                    	<li key={book.id}>
                       <Book />
-                        
-                     
                       </li>
+                    	))
+                    }
+            
                     </ol>
                   </div>
                 </div>
@@ -38,11 +50,16 @@ class MainPage extends Component {
                   <h2 className="bookshelf-title">Read</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-                      <li>
+                    {
+                    	this.props.books
+                    	.filter(book => book.shelf === 'read')
+                    	.map(book => (
+                    	<li key={book.id}>
                       <Book />
-                        
-                    
                       </li>
+                    	))
+                    }
+      
                     </ol>
                   </div>
                 </div>
